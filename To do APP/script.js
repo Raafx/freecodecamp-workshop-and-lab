a50acbd61c64b8fd959d5a1f08da88d0daea6728
Raafx <ahmadrafiin467@gmail.com>
2025-10-01 19:31:09 +0800

    add edit and delete task feuture

--- To do APP/script.js
+++ To do APP/script.js
@@ -18,11 +18,18 @@ openTaskFormBtn.addEventListener("click", () =>
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-  if(titleInput.value !== "" || dateInput.value !== "" || descriptionInput.value !== "" ){
+
+  const formInputsContainValues = titleInput.value !== "" || dateInput.value !== "" || descriptionInput.value !== ""
+
+  const formInputValuesUpdated = titleInput.value != currentTask.title || dateInput.value != currentTask.date || descriptionInput.value != currentTask.description
+
+  if(formInputsContainValues && formInputValuesUpdated){
     confirmCloseDialog.showModal()
   } else {
     reset()
   }
+
+  
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
@@ -44,6 +51,8 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj
   }
 
   updateTaskContainer()
@@ -58,8 +67,8 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
+          <button type="button" onclick="editTask(this)" class="btn">Edit</button>
+          <button type="button" onclick="deleteTask(this)" class="btn">Delete</button>
         </div>
       `
     }
@@ -71,6 +80,30 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addOrUpdateTask()
 });
+
+
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(item => item.id === buttonEl.parentElement.id);
+  buttonEl.parentElement.remove()
+  taskData.splice(dataArrIndex,1)
+
+  
+  
+}
+
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(item => item.id === buttonEl.parentElement.id);
+  currentTask = taskData[dataArrIndex]
+  
+  titleInput.value = currentTask.title
+  dateInput.value = currentTask.date
+  descriptionInput.value = currentTask.description
+
+  addOrUpdateTaskBtn.innerText = "Update Task"
+
+  taskForm.classList.toggle("hidden")
+}
 
 const reset = () => {
     titleInput.value = ""

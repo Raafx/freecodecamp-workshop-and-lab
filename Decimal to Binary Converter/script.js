e25d8ed205f6c20e1a452d5631da8e74b7ee61c2
Raafx <ahmadrafiin467@gmail.com>
2025-12-17 16:40:18 +0800

    Decimal To Binary Converter COMPLETED

--- Decimal to Binary Converter/script.js
+++ Decimal to Binary Converter/script.js
@@ -4,6 +4,37 @@ const convertBtn = document.getElementById("convert-btn");
 
 const result = document.getElementById("result");
 
+const animationData = [
+    {
+        inputVal:5,
+        addElDelay: 1000,
+        msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+        showMsgDelay: 15000,
+        removeElDelay: 20000
+
+    },
+    {
+        inputVal:2,
+        addElDelay: 1500,
+        msg: "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+        showMsgDelay: 10000,
+        removeElDelay: 15000
+
+    },
+    {   
+        msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+        showMsgDelay: 5000,
+        removeElDelay: 10000,
+        inputVal:1,
+        addElDelay: 2000
+
+    },
+
+
+]
+
+const animationContainer = document.getElementById("animation-container") 
+
 const checkUserInput = () => {
 
     let inputInt = parseInt(numberInput.value)
@@ -22,10 +53,27 @@ const checkUserInput = () => {
 }
 
 const showAnimation = () => {
-    console.log("free")
-    setTimeout(() => console.log("Code"),1000)
-    console.log("Camp")
+    result.innerText = "Call Stack Animation";
+
+    animationData.forEach(obj => {
+        setTimeout(() => {
+            animationContainer.innerHTML += `<p id="${obj.inputVal}" class="animation-frame">decimalToBinary(${obj.inputVal})</p>`
+        }, obj.addElDelay)
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).
+            textContent = obj.msg
+        }, obj.showMsgDelay)
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove()
+        }, obj.removeElDelay)
+    })
+
+    setTimeout(() => {
+        result.textContent = decimalToBinary(5)
+    },20000);
+    
 }
+
 
 const decimalToBinary = (input) => {
     if(input === 0 || input === 1){
